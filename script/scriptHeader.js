54d116d450a573ead5c1649d886016db95d9ed37
Amanuel-JuniorT <amanueltzb@gmail.com>
2023-12-25 02:35:27 -0800

    Search bar

--- script/scriptHeader.js
+++ script/scriptHeader.js
@@ -8,9 +8,15 @@ toLogin = document.querySelector('.switch-to-login'),
 toSignup = document.querySelector('.switch-to-signup'),
 dis_prev = document.querySelector('.dis-prev'),
 dis_next = document.querySelector('.dis-next'),
-discounts = document.querySelector('.discount_collection')
+discounts = document.querySelector('.discount_collection'),
+all_parts = document.querySelector('.all-parts'),
+parts = all_parts.querySelectorAll('li'),
+search = document.querySelector('.search')
 ;
 
+window.addEventListener('click', ()=>{
+  all_parts.classList.remove('active');
+})
 
 login.addEventListener('click', ()=>{
   register_bg.classList.add('active');
@@ -37,3 +43,61 @@ toSignup.addEventListener('click', ()=>{
   loginPage.classList.remove('active'); 
   signupPage.classList.add('active');
 })
+
+search.addEventListener('click', ()=>{
+  // if(!all_parts.classList.contains('active')){
+  //   all_parts.classList.add('active');
+  // }
+})
+// search.addEventListener('keyup', e =>{
+//   parts.forEach(part =>{
+//     if(part.textContent.toLowerCase().includes(e.target.value.toLowerCase())){
+//       part.style.display = "block";
+//     } else{
+//       part.style.display = "none";
+//     }
+//     if(!all_parts.classList.contains('active')){
+//       all_parts.classList.add('active');
+//     }
+//     if(e.key == "Escape"){
+//       all_parts.classList.remove('active');
+//       search.blur()
+//     }
+    
+//   })
+// })
+
+
+const arr = {
+  catagory: ['Car Parts', 'Oils and Fluids', 'Accessories and Equipments'],
+  item: ['Spark Plug', 'Break']
+}
+
+search.addEventListener('keyup', e =>{
+  all_parts.innerHTML = '';
+  arr.catagory.forEach(cat => render(cat, e))
+  arr.item.forEach(it =>render(it, e))
+  
+  if(search.value === ''){
+    all_parts.innerHTML = '';
+  }
+  if(!all_parts.classList.contains('active')){
+    all_parts.classList.add('active');
+  }
+  if(all_parts.innerHTML==''){
+    all_parts.classList.remove('active');
+  } 
+  if(e.key == "Escape"){
+    all_parts.classList.remove('active');
+    search.blur()
+  }
+})
+
+function render(given, e){
+  if(given.toLowerCase().includes(e.target.value.toLowerCase())){
+    let itemLists = document.createElement('li');
+    itemLists.innerText = given;
+    all_parts.append(itemLists);
+  }
+  
+}
